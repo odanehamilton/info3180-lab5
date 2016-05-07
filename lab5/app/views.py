a950501ddfe8b78ba88dcfff057496169b43502c
Odane P. Hamilton <odane.hamilton@gmail.com>
2016-05-07 20:24:12 +0000

    Added files

--- lab5/app/views.py
+++ lab5/app/views.py
@@ -83,18 +83,6 @@ def logout():
     return redirect(url_for('home'))
 
     
-    
-    #if g.user is not None and g.user.is_authenticated:
-     #   return redirect(url_for('index'))
-    #form = LoginForm()
-   # if form.validate_on_submit():
-  #      session['remember_me'] = form.remember_me.data
- #       return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-#    return render_template('login.html', 
-     #                      title='Sign In',
-    #                       form=form,
-   #                        providers=app.config['OPENID_PROVIDERS'])
-
 @app.route('/')
 def home():
     """Render website's home page."""
